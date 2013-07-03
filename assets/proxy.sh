8cf36da37511c68152e40c2bfeda9a0d835fc605
Maksim Lin <maks@manichord.com>
2013-07-04 09:50:14 +1000

    Initial commit
    as downloaded from http://forum.xda-developers.com/showthread.php?t=766569

---
+++ assets/proxy.sh
@@ -0,0 +1,95 @@
+#!/system/bin/sh
+
+DIR=$2
+type=$3
+host=$4
+port=$5
+auth=$6
+user=$7
+pass=$8
+domain=$9
+
+PATH=$DIR:$PATH
+
+case $1 in
+ start)
+
+echo "
+base {
+ log_debug = off;
+ log_info = off;
+ log = stderr;
+ daemon = on;
+ redirector = iptables;
+}
+" >$DIR/redsocks.conf
+
+ case $type in
+  http)
+ case $auth in
+  true)
+  echo "
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8123;
+ ip = $host;
+ port = $port;
+ type = http-relay;
+ login = \"$user\";
+ password = \"$pass\";
+ domain = \"$domain\";
+} 
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8124;
+ ip = $host;
+ port = $port;
+ type = http-connect;
+ login = \"$user\";
+ password = \"$pass\";
+ domain = \"$domain\"; 
+} 
+" >>$DIR/redsocks.conf
+   ;;
+   false)
+   echo "
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8123;
+ ip = $host;
+ port = $port;
+ type = http-relay;
+} 
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8124;
+ ip = $host;
+ port = $port;
+ type = http-connect;
+} 
+ " >>$DIR/redsocks.conf
+   ;;
+ esac
+   ;;
+  socks)
+  echo "
+redsocks {
+ local_ip = 127.0.0.1;
+ local_port = 8123;
+ ip = $host;
+ port = $port;
+ type = socks5;
+ }
+ " >>$DIR/redsocks.conf
+   ;;
+ esac
+
+ $DIR/redsocks -p $DIR/redsocks.pid -c $DIR/redsocks.conf
+ ;;
+stop)
+  kill `cat $DIR/redsocks.pid`
+  
+  rm $DIR/redsocks.pid
+  
+  rm $DIR/redsocks.conf
+esac
