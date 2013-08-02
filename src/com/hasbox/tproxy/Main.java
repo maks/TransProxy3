d2b8e0535885464d4c04d3347ae19becdc00e0bf
Maksim Lin <maks@manichord.com>
2013-08-02 12:46:48 +1000

    fix problems with char escaping in M$ AD style usernames
    so removed shell scripts and now writeout redsock config directly
    from Java code to avoid problems with backslash char escaping in
    shell. Also need to doubel up backslashes in usernames as redsocks
    user sprintf() which needs any backslash escaped with another backslash.
    
    and since we did all that, got rid of shell scripts and just start/stop
    redsock directly form java. also for now check if redsock is running using
    pid file as checking the socket doesnt work when its called from maain UI
    thread!!

--- src/com/hasbox/tproxy/Main.java
+++ src/com/hasbox/tproxy/Main.java
@@ -20,14 +20,19 @@ import com.hasbox.tproxy.ShellCommand.CommandResult;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Formatter;
 
 public class Main extends PreferenceActivity {
 	public static final String PREFS_NAME = "prefs";
+    private static final int REDSOCKS_HTTP_PORT = 8123;
+    private static final String LOGTAG = Main.class.getSimpleName();
+    private static final String REDSOCK_PID_FILE = "redsocks.pid";
 	final int START = 1;
 	final int STOP = 2;
 	String basedir = null;
@@ -52,8 +57,9 @@ public class Main extends PreferenceActivity {
 		 basedir = getBaseContext().getFilesDir().getAbsolutePath();
 		} catch (Exception e) {}
 
-		copyfile("redsocks");
-		copyfile("proxy.sh");
+		copyfile("redsocks-armv71");
+		copyfile("redsocks-i686");
+		copyfile("runproxy.sh");
 		copyfile("redirect.sh");
 
 		super.onCreate(savedInstanceState);
@@ -82,12 +88,14 @@ public class Main extends PreferenceActivity {
 			public boolean onPreferenceChange( Preference preference,  Object newValue) {	
                 final CheckBoxPreference cbp = (CheckBoxPreference) preference;
                 	Boolean ret = proxy((Boolean)newValue ? START : STOP);
-            		setenabled(checklistener());
+            		//setenabled(checklistener());
+                	setenabled(checkPIDFile());
 					return ret;
 			}
 		});
 		
-		setenabled(checklistener());
+		//setenabled(checklistener());
+		setenabled(checkPIDFile());
 	}
 
 	public boolean proxy(int action) {
@@ -98,7 +106,10 @@ public class Main extends PreferenceActivity {
 			String host = settings.getString("proxyHost", "");
 			String port = settings.getString("proxyPort", "");
 			Boolean auth = settings.getBoolean("isAuthEnabled", false);
-			String user = settings.getString("username", "");
+			// need to make sure we escape any slashes in username & passwd fields
+			// as redsocks then passes them through sprintf before sending in HTTP header  
+			String user = settings.getString("username", "").replace("\\", "\\\\");
+			Log.d("TPROXY", "user:"+user);
 			String pass = settings.getString("password", "");
 			String proxy_type = settings.getString("proxyType", "http");
 			
@@ -131,33 +142,76 @@ public class Main extends PreferenceActivity {
 				alert("Cannot resolve hostname "+host, null); 
 				return false;
 			}
-				Log.v("tproxy","proxy.sh start " + basedir + " "
-						+"type=" + proxy_type + " "
-						+"host=" + ipaddr + " "
-						+"port=" + port.trim() + " "
-						+"auth=" + auth + " "
-						+"user=" + user.trim() + " "
-						+"pass=*****"
-						+ "arch="+arch);
-				
-				 ShellCommand cmd = new ShellCommand();
-				 CommandResult r = cmd.sh.runWaitFor(basedir+"/proxy.sh start " + basedir
-						 	+ " " + proxy_type
-						 	+ " " + ipaddr
-							+ " " + port.trim()
-							+ " " + auth
-							+ " " + user.trim()
-							+ " " + pass.trim()
-							+ " " + arch);
+			Log.v("tproxy","proxy.sh start " + basedir + " "
+					+"type=" + proxy_type + " "
+					+"host=" + ipaddr + " "
+					+"port=" + port.trim() + " "
+					+"auth=" + auth + " "
+					+"user=" + user.trim() + " "
+					+"pass=*****"
+					+"arch="+arch);
+			
+            String preface = "base { \n" +
+                    "log_debug = off; \n" +
+                    "log_info = off; \n" +
+                    "log = stderr; \n" +
+                    "daemon = on; \n" +
+                    "redirector = iptables; \n" +
+                    "} \n";
+			
+			String template1 = "redsocks { \n"+
+			    "local_ip = 127.0.0.1;\n"+
+			    "local_port = "+REDSOCKS_HTTP_PORT+";\n"+
+			    "ip = %s;\n"+
+			    "port = %s;\n"+
+			    "type = http-relay;\n"+
+			    "login = \"%s\";\n"+
+			    "password = \"%s\";\n"+
+			   "}\n"; 
+			String template2 = "redsocks {\n"+
+			    "local_ip = 127.0.0.1;\n"+
+			    "local_port = 8124;\n"+
+			    "ip = %s;\n"+
+			    "port = %s;\n"+
+			    "type = http-connect;\n"+
+			    "login = \"%s\";\n"+
+			    "password = \"%s\";\n"+
+			   "}\n";
+			
+			try {
+			    Log.d("TPROXY", "CONF FILE: "+basedir);
+			    File f = new File(basedir+"/redsocks.conf");
+			    f.createNewFile();
+			    Log.d("TPROXY", "EXISTS"+f.exists());
+                FileWriter writer = new FileWriter(f);
+                String out1 = String.format(template1, ipaddr, port.trim(), user.trim(), pass.trim());
+                String out2 = String.format(template2, ipaddr, port.trim(), user.trim(), pass.trim());
+                writer.write(preface+out1+out2);
+                writer.close();
+            } catch (IOException e) {
+                Log.e("TPROXY", "", e);
+            }
+			
+			ShellCommand cmd = new ShellCommand();
+			//CommandResult r = cmd.sh.runWaitFor(basedir+"/runproxy.sh start " + basedir +" " + arch);
+			CommandResult r = cmd.sh.runWaitFor(basedir+"/redsocks-"+arch+" -p "+basedir+"/"+REDSOCK_PID_FILE+" -c "+basedir+"/redsocks.conf" );
 
-				 if (!r.success()) {
-					    Log.v("tproxy", "Error starting proxy.sh (" + r.stderr + ")");
-						cmd.sh.runWaitFor(basedir+"/proxy.sh stop "+ basedir);
-						alert ("Failed to start proxy.sh ("+ r.stderr + ")", null);
-					    return false;
-				} 
-				 
-			if (checklistener()) {
+			if (!r.success()) {
+			    Log.v("tproxy", "Error starting proxy.sh (" + r.stderr + ")");
+				cmd.sh.runWaitFor(basedir+"/proxy.sh stop "+ basedir);
+				alert ("Failed to stop proxy.sh ("+ r.stderr + ")", null);
+			    return false;
+			}
+			
+			//FIXME: HACK, do this properly by having call back once redsocks pid file is detected!
+			try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+			
+			if (checkPIDFile()) {
 				 r = cmd.su.runWaitFor(basedir+"/redirect.sh start " + proxy_type);
 				 
 				 if (!r.success()) {
@@ -176,7 +230,10 @@ public class Main extends PreferenceActivity {
 		} else { // stop tproxy
 		 	Log.v("tproxy", "Successfully ran redirect.sh stop");
 			ShellCommand cmd = new ShellCommand();
-			cmd.sh.runWaitFor(basedir+"/proxy.sh stop "+basedir);
+			//cmd.sh.runWaitFor(basedir+"/proxy.sh stop "+basedir);
+			cmd.sh.runWaitFor("kill `cat "+basedir+"/redsocks.pid`");
+			new File(basedir, "redsocks.pid").delete();
+			new File(basedir, "redsocks.conf").delete();
 			cmd.su.runWaitFor(basedir+"/redirect.sh stop");
 			return true;
 		}
@@ -219,22 +276,29 @@ public class Main extends PreferenceActivity {
 				}).show();		
 	}
 	
-	public boolean checklistener() {
-		Socket socket = null;
-		try {
-			socket = new Socket("127.0.0.1", 8123);
-		} catch (Exception e) {
-		}
-
-		if (socket != null && socket.isConnected()) {
-			try {
-				socket.close();
-			} catch (Exception e) {
-			}
-			return true;
-		} else {
-			return false;
-		}
+// FIXME: needs to run on background Thread !!!	
+//	public boolean checklistener() {
+//		Socket socket = null;
+//		try {
+//			socket = new Socket("127.0.0.1", REDSOCKS_HTTP_PORT);
+//		} catch (Exception e) {
+//		    Log.e(LOGTAG, "could not connect to redsocks http port:"+REDSOCKS_HTTP_PORT, e);
+//		}
+//
+//		if (socket != null && socket.isConnected()) {
+//			try {
+//				socket.close();
+//			} catch (Exception e) {
+//			}
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+	
+	public boolean checkPIDFile() {
+	    File pidfile = new File(basedir, REDSOCK_PID_FILE);
+	    return pidfile.exists(); 
 	}
 	
 	public void setenabled(boolean b) {
